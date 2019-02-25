ebe73f03abf7d33b1fdd8bfbc96c91850bad6bd6
Sergei Kolesin <Sergei_Kolesin@epam.com>
2019-02-25 18:49:32 +0300

    Initial Commit

---
+++ MakeathonDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MakeathonDemo
+//
+//  Created by Sergei Kolesin on 2/25/19.
+//  Copyright © 2019 Sergei Kolesin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MakeathonDemoTests/MakeathonDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  MakeathonDemoTests.swift
+//  MakeathonDemoTests
+//
+//  Created by Sergei Kolesin on 2/25/19.
+//  Copyright © 2019 Sergei Kolesin. All rights reserved.
+//
+
+import XCTest
+@testable import MakeathonDemo
+
+class MakeathonDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
